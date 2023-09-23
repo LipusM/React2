5db93590b8d02a8dd0d172a93b84c26908de6f18
LipusM <mlipus007@gmail.com>
2023-09-23 13:25:59 +0200

    Smazání zakomentovaného kódu

--- Cviceni/nabytekXXXMuckL1/src/main.jsx
+++ Cviceni/nabytekXXXMuckL1/src/main.jsx
@@ -1,39 +1,3 @@
-/* import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-import App from './App.jsx'
-import HomePage from './pages/HomePage/index.jsx'
-import ProductPage from './pages/ProductPage/index.jsx'
-
-import './index.scss'
-
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-        children: [
-          {
-            path: "/product/:id",
-            element: <ProductPage />
-          }
-        ]
-      },
-    ]
-  }
-])
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
- */
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
