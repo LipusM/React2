fd88b45cdc226c35d09a48cd8708157468a94a17
LipusM <mlipus007@gmail.com>
2023-09-23 13:53:10 +0200

    Zarovnávání komponenty ProductPage

--- Cviceni/nabytekXXXMuckL1/src/pages/ProductPage/index.jsx
+++ Cviceni/nabytekXXXMuckL1/src/pages/ProductPage/index.jsx
@@ -4,27 +4,31 @@ import "./style.scss";
 
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Loader from "../../components/Loader";
 
 const ProductPage = () => {
+  const { id } = useParams();
 
-  const { id } = useParams();  
-
-  const [productInfo, setProductInfo] = useState({})  
+  const [productInfo, setProductInfo] = useState({});
 
   useEffect(() => {
     fetch(`https://apps.kodim.cz/react-2/xxxmuck/products/${id}`)
-    .then(response => response.json())
-    .then(data => 
-        setProductInfo(data)
-    )
-  }, [])
+      .then((response) => response.json())
+      .then((data) => setProductInfo(data));
+  }, []);
 
   return (
-    <div>
-            <h2>{productInfo.name}</h2>
-          <img src={productInfo.image} alt={productInfo.name} />
+    <div className="chosenProduct">
+      <div className="productImage">
+        <img src={productInfo.image} alt={productInfo.name} />
+      </div>
+      <div className="productInfo">
+        <h2>{productInfo.name}</h2>
+        <button>Objednat</button>
+      </div>
     </div>
   );
 };
 
 export default ProductPage;
+
