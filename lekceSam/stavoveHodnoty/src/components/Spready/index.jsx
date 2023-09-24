dd0aaa442365d3a7263d3d15dfdb564c31933a19
LipusM <mlipus007@gmail.com>
2023-09-24 15:54:56 +0200

    ..

--- lekceSam/stavoveHodnoty/src/components/Spready/index.jsx
+++ lekceSam/stavoveHodnoty/src/components/Spready/index.jsx
@@ -2,10 +2,13 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
+const x = [1, 2, 3]
+x.unshift(8)
+
 const Spready = () => {
     return (
         <>
-        
+            {c(x)}
         </>
     )
 }
