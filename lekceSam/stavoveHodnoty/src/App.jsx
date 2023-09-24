ec70cffb3b164e4ca91542110d20405cf06b60cc
LipusM <mlipus007@gmail.com>
2023-09-24 15:14:44 +0200

    ..

--- lekceSam/stavoveHodnoty/src/App.jsx
+++ lekceSam/stavoveHodnoty/src/App.jsx
@@ -1,12 +1,15 @@
+const c = console.log.bind(document)
 
 import './app.scss'
+
+import Motivace from './components/Motivace'
 
 const App = () => {
 
 
   return (
     <>
-
+      <Motivace/>
     </>
   )
 }
---
+++ lekceSam/stavoveHodnoty/src/components/Motivace/index.jsx
@@ -0,0 +1,36 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+
+const age1 = 25;
+const age2 = 25;
+
+const name1 = 'petr';
+const name2 = 'petr';
+
+const workout1 = [5, 3, 7];
+const workout2 = [5, 3, 7];
+
+const x = [1, 2, 3]
+const y = x
+x.push(4)
+
+const Motivace = () => {
+    return (
+        <>
+        {c("Věk:")}
+        {c(age1 === age2)}
+
+        {c("Jména:")}
+        {c(name1 === name2)}
+
+        {c("Cvičení:")}
+        {c(workout1 === workout2)}
+
+        {c("Proměnné x a y:")}
+        {c(x === y)}
+        </>
+    )
+}
+
+export default Motivace
