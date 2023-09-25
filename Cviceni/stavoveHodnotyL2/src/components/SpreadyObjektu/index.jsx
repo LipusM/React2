c761744ab489f580c4f6ba09da1c4d2f0a76ea63
LipusM <mlipus007@gmail.com>
2023-09-25 22:32:11 +0200

    ..

--- Cviceni/stavoveHodnotyL2/src/components/SpreadyObjektu/index.jsx
+++ Cviceni/stavoveHodnotyL2/src/components/SpreadyObjektu/index.jsx
@@ -1,9 +1,29 @@
 const c = console.log.bind(document)
 
+//Zadání (všechny operace proveďte jako immutable)
+const ticket = { 
+    from: 'Praha', 
+    to: 'Lisabon', 
+    currency: 'EUR', 
+    price: 115 
+    }
+
+//1) Přidejte do objektu vlastnost duration s hodnotou 2h
+const ticket1 = {...ticket, duration: "2h"}
+
+//2) Změňte vlastnost price na hodnotu 130
+const ticket2 = {...ticket, price: 130}
+
 const SpreadyObjektu = () => {
     return (
         <>
-        
+        {c("Úkol číslo 1:")}
+        {c(ticket1)}
+        {c(ticket)}
+
+        {c("Úkol číslo 2:")}
+        {c(ticket2)}
+        {c(ticket)}
         </>
     )
 }
