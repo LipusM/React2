b01ad4d413e68998e9ea12ad826618ba16f3b61e
LipusM <mlipus007@gmail.com>
2023-09-25 18:18:23 +0200

    ..

--- lekceSam/stavoveHodnoty/src/components/Spready/index.jsx
+++ lekceSam/stavoveHodnoty/src/components/Spready/index.jsx
@@ -3,12 +3,16 @@ const c = console.log.bind(document)
 import "./style.scss"
 
 const x = [1, 2, 3]
-x.unshift(8)
+x.unshift(0)
 
 const Spready = () => {
     return (
         <>
+            {c("Mutable, resp. na místě:")}
             {c(x)}
+
+            {c("Immutable")}
+            {c([...x, 4])}
         </>
     )
 }
