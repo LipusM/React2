202270bc950bff043effa91c50f2a587536bc309
LipusM <mlipus007@gmail.com>
2023-09-25 21:49:10 +0200

    ..

--- Cviceni/stavoveHodnotyL2/src/App.jsx
+++ Cviceni/stavoveHodnotyL2/src/App.jsx
@@ -1,15 +1,17 @@
 const c = console.log.bind(document)
 
-import './app.scss'
+import "./app.scss"
+
+import SpreadyPoli from './components/SpreadyPoli'
 
 const App = () => {
 
   return (
     <>
-
+      <SpreadyPoli/>
     </>
   )
-  
+
 }
 
 export default App
---
+++ Cviceni/stavoveHodnotyL2/src/components/SpreadyPoli/index.jsx
@@ -0,0 +1,23 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+
+
+//Zadání (všechny operace proveďte jako immutable)
+const names = ['jana', 'pavel', 'eva', 'jitka', 'radek', 'zuzka', 'ondra']
+
+// 1) Přidejte na konec pole hodnotu "hanka"
+// 2) Přidejte na začátek hodnotu "romana"
+// 3) Vytvořte čiskou kopii pole
+// 4) Odstraňte poslední prvek pole
+// 5) Odstraňte z pole hodnotu na třetím indexu
+
+const SpreadyPoli = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default SpreadyPoli
