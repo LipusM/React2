bbb37a2ceed94716662a2464423ce93abed29395
LipusM <mlipus007@gmail.com>
2023-09-25 23:22:02 +0200

    ..

--- Cviceni/stavoveHodnotyL2/src/components/Imutabilita/index.jsx
+++ Cviceni/stavoveHodnotyL2/src/components/Imutabilita/index.jsx
@@ -1,9 +1,29 @@
 const c = console.log.bind(document)
 
+// Námi vytvořené pole
+const array1 = ["Jedna", "Dvě", "Tři", "Čtyři", "Pět"]
+
+//1) Vytvořte funkci reversed, která obrátí pořadí prvků v poli.
+const reversed = (array) => {
+    return [...array].reverse()
+}
+
+//2) Vytvořte funkci remove, která obdrží pole a index. Funkce smaže prvek na zadaném indexu
+const remove = (array, index) => {
+    const newArray = [...array]
+    return newArray.splice(index, 1)
+
+    /* return newArray */
+}
+
 const Imutabilita = () => {
     return (
         <>
-        
+            {c("Úkol číslo 1:")}
+            { c(reversed(array1)) }
+
+            {c("Úkol číslo 2:")}
+            { c(remove(array1, 2)) }
         </>
     )
 }
