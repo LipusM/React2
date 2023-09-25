ac9bbc4b4820273ae84bedc5421804be696600a0
LipusM <mlipus007@gmail.com>
2023-09-25 19:58:42 +0200

    ..

--- lekceSam/stavoveHodnoty/src/components/SpreadyObjektu/index.jsx
+++ lekceSam/stavoveHodnoty/src/components/SpreadyObjektu/index.jsx
@@ -12,19 +12,34 @@ const time2 = {
     mins: 25
 }
 
+const time3 = {
+    hours: 5,
+    mins: 21
+}
+
 const SpreadyObjektu = () => {
     return (
         <>
+            {c("3: //////////")}
             {c("1: Nemodifikovaný objekt:")}
             {c(time1)}
 
             {c("2: Aplikace spreadu na objekt:")}
             {c({...time1, secs: 35})}
 
-            {c("//////////")}
+            {c("2: //////////")}
             {c(time2)}
             {c({...time2, hours: 10})}
             {c({hours: 10, ...time2})}
+
+            {c("3: //////////")}
+            {c(time3)}
+            {time3.hours = 10}
+            {time3.secs = 35}
+            {c(time3)}
+
+            {{ ...time3, hours: 10, secs: 35 }}
+
         </>
     )
 }
