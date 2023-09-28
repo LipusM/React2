1e896a8180b58ede09ae208b7bc37b40526c8c19
LipusM <mlipus007@gmail.com>
2023-09-28 12:08:12 +0200

    Úprava komponenty LadyBug, aby brala 1no props

--- Cviceni/L2/ladybug-zadani-main/src/components/Ladybug/index.jsx
+++ Cviceni/L2/ladybug-zadani-main/src/components/Ladybug/index.jsx
@@ -1,9 +1,31 @@
-const c = console.log.bind(document)
+/* const c = console.log.bind(document)
 
 import React from 'react';
 import './style.css';
 
 const Ladybug = ({ posX, posY, orientation }) => {
+  return (
+    <div 
+      className={`ladybug ladybug--${orientation}`} 
+      style={{
+        top: `${posX}px`,
+        left: `${posY}px`,
+      }}
+    />
+  );
+};
+
+export default Ladybug; */
+
+const c = console.log.bind(document)
+
+import React from 'react';
+import './style.css';
+
+const Ladybug = ({props}) => {
+
+  const {posX, posY, orientation} = props
+
   return (
     <div 
       className={`ladybug ladybug--${orientation}`} 
