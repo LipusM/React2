caeaa36c6d5ef97ec9b3bb23e2fa51f776ffcc9a
LipusM <mlipus007@gmail.com>
2023-09-28 10:56:25 +0200

    ..

--- lekceSam/stavoveHodnoty/src/components/React/index.jsx
+++ lekceSam/stavoveHodnoty/src/components/React/index.jsx
@@ -2,10 +2,26 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
+
+const TimeDisplay1 = () => {
+    const [hours, setHours] = useState(13);
+    const [minutes, setMinutes] = useState(24);
+    const [seconds, setSeconds] = useState(58);
+  };
+
+
+  const TimeDisplay2 = () => {
+    const [time, setTime] = useState({
+      hours: 13,
+      minutes: 24,
+      seconds: 58,
+    });
+  };
+
 const React = () => {
     return(
         <>
-        
+            
         </>
     )
 }
