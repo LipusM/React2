28d08d3c8dd6c3a9aa3b4c162caefb3eb5677e1c
LipusM <mlipus007@gmail.com>
2023-10-16 22:53:40 +0200

    Stylování

--- Cviceni/datluj.czL4/src/components/StageTime/index.jsx
+++ Cviceni/datluj.czL4/src/components/StageTime/index.jsx
@@ -1,7 +1,7 @@
 const c = console.log.bind(document)
 
 import React, { useState, useEffect } from 'react';
-import Wordbox from '../Wordbox';
+import WordboxTime from '../WordboxTime';
 import wordList from '../../word-list';
 
 import './style.scss'
@@ -21,14 +21,20 @@ const generateWord = (size) => {
   };
   
   const StageTime = () => {
+    //Iniciální nastavení slov
     const [words, setWords] = useState(["jahoda", "klavesnice", "kolac"])
 
+    //Vyhodnocování psaní
     const [evaulation, setEvaluation] = useState({
       mistakes: 0,
       writtenWords: 0
     })
     const {mistakes, writtenWords} = evaulation
 
+    //Nastavení času hry
+    const [timer, setTimer] = useState(0)
+
+    //Odstranění prvního slova po napsání a generování nového na konec
     const handleFinish = () => {
       const copy = [...words]
       copy.splice(0,1) //Odstranění prvního elementu pole
@@ -37,21 +43,59 @@ const generateWord = (size) => {
       setWords(copy)
     }
 
+    //Vyhodnocuje počet chyb a napsaných slov
     const onEvaulation = (mistake, word) => {
       setEvaluation({...evaulation, mistakes: mistakes + mistake, writtenWords: writtenWords + word})
+    }
+
+    //Spuštění časovače
+    useEffect(() => {
+
+      if(timer !== 0){
+        startGame(timer)
+      }
+
+    }, [timer])
+
+    const startGame = (chosenTime) => {
+      setTimer(chosenTime)
+
+     /*  if(chosenTime === 60){
+        c("jedna minuta")
+      } else if (chosenTime === 120){
+        c("dvě minuty")
+      } else if (chosenTime === 300) {
+        c("pět minut")
+      } */
+    
+      if(chosenTime === 60){
+        const start = setInterval( (() => setTimer(prev => {
+          c(timer)
+          if(timer === 0){
+            clearInterval(start) 
+          }
+            prev - 1
+        })), 1000 )
+      }
+
+    }
+
+    const gameTime = (chosenTime) => {
+      setTimer(chosenTime => chosenTime - 1) 
+      c(timer)
     }
   
     return (
       <div className="stage">
         <div className="stage__time">
-            <div>1 minuta</div>
+            <div onClick={() => setTimer(60)}>1 minuta</div>
             |
-            <div>2 minuty</div>
+            <div onClick={() => setTimer(120)}>2 minuty</div>
             |
-            <div>5 minut</div>
+            <div onClick={() => setTimer(300)}>5 minut</div>
         </div>
         <div className="stage__words">
-          {words.map((word, index) => <Wordbox key={word} word={word} onFinish={handleFinish} 
+          {words.map((word, index) => <WordboxTime key={word} word={word} onFinish={handleFinish} 
           active={index === 0 && true} evaluate={onEvaulation} firstWord={index === 0 ? "active-word" : "non-active-word"}/>)}
         </div>
       </div>
