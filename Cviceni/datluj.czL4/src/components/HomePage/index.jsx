5a02fed1b8d35928417af0ed7588ca2bc987ddd7
LipusM <mlipus007@gmail.com>
2023-10-16 19:09:03 +0200

    React router

---
+++ Cviceni/datluj.czL4/src/components/HomePage/index.jsx
@@ -0,0 +1,16 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+import { NavLink } from "react-router-dom"
+
+const HomePage = () => {
+    return(
+        <section id="intro-choose">
+            <NavLink className={"chosen-game"} to="/hra">Bez časového omezení</NavLink>
+            
+            <NavLink className={"chosen-game"} to="/hra">S časovým omezením</NavLink>
+        </section>
+    )
+}
+
+export default HomePage
