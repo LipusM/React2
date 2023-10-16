b1e31a6cbc7e9b13aa1ea59667dd42a48864ce07
LipusM <mlipus007@gmail.com>
2023-10-16 19:09:39 +0200

    Importy komponent

--- Cviceni/datluj.czL4/src/App.jsx
+++ Cviceni/datluj.czL4/src/App.jsx
@@ -1,16 +1,18 @@
 const c = console.log.bind(document);
 
 import "./app.scss";
-import { useState } from "react";
-
-import Stage from "./components/Stage";
+import { Outlet } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 const App = () => {
 
   return (
     <div className="container">
-      <h1>Datlování</h1>
-      <Stage />
+      {/* <h1>Datlování</h1> */}
+      <Header />
+      <HomePage />
+        <Outlet />
     </div>
   )
 
