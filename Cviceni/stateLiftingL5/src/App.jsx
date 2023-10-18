b4f7c2c00030971ec53c39c6686108cb3fed8eba
LipusM <mlipus007@gmail.com>
2023-10-18 18:17:51 +0200

    Přenesení

---
+++ Cviceni/stateLiftingL5/src/App.jsx
@@ -0,0 +1,19 @@
+const c = console.log.bind(document)
+
+import Banner from './components/Banner'
+import Cart from './components/Cart'
+import Header from './components/Header'
+import './app.scss'
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <Header />
+      <Banner />
+      <Cart />
+  </div>
+  )
+}
+
+export default App
