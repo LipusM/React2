e0c912e8a673ad28b3c5dcdbbc5ca54d78e771a2
LipusM <mlipus007@gmail.com>
2023-10-19 19:30:07 +0200

    ..

--- lekce/stateLiftingL5/src/components/Cart/index.jsx
+++ lekce/stateLiftingL5/src/components/Cart/index.jsx
@@ -39,16 +39,22 @@ const Cart = () => {
     setCartProducts(newProducts)
   }
 
+  const cartAmount = () => {
+
+
+    return cartProducts.reduce((sum, product) => sum += product.amount, 0)
+  }
+
   return (
     <div className="cart">
       <div className="cart__head">
         <h2>Košík</h2>
-        <span>Položek: 1</span>
+        <span>Položek: {cartAmount()}</span>
       </div>
       <div className="cart__items">
         {cartProducts.map((product, index) => (
           <CartItem product={product} 
-          onAmountChange={(amount) => {handleAmountChange(index, amount)}} />
+          onAmountChange={(newAmount) => {handleAmountChange(index, newAmount)}} />
         ))}
       </div>
     </div>
