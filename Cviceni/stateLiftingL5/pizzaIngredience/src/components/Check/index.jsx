01d6e90f890dbacd84fd6e04ff8032b73943c6c8
LipusM <mlipus007@gmail.com>
2023-10-20 18:43:34 +0200

    Povýšení stavu o jednu úroveň.

--- Cviceni/stateLiftingL5/pizzaIngredience/src/components/Check/index.jsx
+++ Cviceni/stateLiftingL5/pizzaIngredience/src/components/Check/index.jsx
@@ -1,19 +1,13 @@
 const c = console.log.bind(document)
 
-import { useState } from 'react'
 import './style.scss'
 
-const Check = () => {
-  const [checked, setChecked] = useState(false)
-  
-  const handleClick = () => {
-    setChecked(!checked);
-  }
+const Check = ({checked, onChange}) => {
   
   return (
     <button 
       className="check"
-      onClick={handleClick}
+      onClick={onChange}
     >
       {checked ? '✓' : ''}
     </button>
--- Cviceni/stateLiftingL5/pizzaIngredience/src/components/Topping/index.jsx
+++ Cviceni/stateLiftingL5/pizzaIngredience/src/components/Topping/index.jsx
@@ -3,10 +3,18 @@ const c = console.log.bind(document)
 import Check from "../Check"
 import './style.scss'
 
+import { useState } from "react"
+
 const Topping = ({ topping }) => {
+  const [checked, setChecked] = useState(false)
+  
+  const handleClick = () => {
+    setChecked(!checked);
+  }
+
   return (
     <div className="topping">
-      <Check />
+      <Check checked={checked} onChange={handleClick}/>
       <span className="topping__content">
         {topping.name}: {topping.price} €
       </span>
