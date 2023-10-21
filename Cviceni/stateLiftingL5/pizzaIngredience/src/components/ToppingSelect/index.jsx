40fc478cda300191c030a8808e0f2f03731b8194
LipusM <mlipus007@gmail.com>
2023-10-21 10:37:55 +0200

    Zbývá nastavit správné zobrazování ceny

--- Cviceni/stateLiftingL5/pizzaIngredience/src/components/ToppingSelect/index.jsx
+++ Cviceni/stateLiftingL5/pizzaIngredience/src/components/ToppingSelect/index.jsx
@@ -14,13 +14,24 @@ const ToppingsSelect = ({ toppings }) => {
     selection[index].selected = !selection[index].selected
 
     setChecked(selection)
-    c(selection[index]) //Test, jestli se opravdu vypisuje ten prvek, na který se kliklo a ano, vypisuje se ten, na který se kliklo
+    c(checked) //Test, jestli se opravdu vypisuje ten prvek, na který se kliklo a ano, vypisuje se ten, na který se kliklo
+  }
+
+  //Fce pro vyhodnocení počtu vybraných topping
+  const toppingAmount = () => {
+    return checked.reduce((sum, chosenTopping) => sum += chosenTopping.selected, 0)
+  }
+
+  //Fce pro vyhodnocení celkové ceny vybraných topping
+  const toppingPrice = () => {
+    return checked.reduce((sum, chosenTopping) => sum += chosenTopping.price, 0).toFixed(2)
+
   }
 
   return (
     <>
       <p>Choose as many toppings as you want</p>
-      <p>Selected toppings: 0, total price: 0 Euro</p>
+      <p>Selected toppings: {toppingAmount()}, total price: {toppingPrice()} Euro</p>
         
       <div className="toppings">
         {toppings.map((topping, index) => <Topping topping={topping} checked={checked[index].selected} 
