613a78776f4353a4ffffd46728072e0c1b6c74b7
LipusM <mlipus007@gmail.com>
2023-10-23 20:13:45 +0200

    Práce na třídách

--- Cviceni/datluj.czL4/src/components/TimeButtons/index.jsx
+++ Cviceni/datluj.czL4/src/components/TimeButtons/index.jsx
@@ -1,14 +1,16 @@
 const c = console.log.bind(document)
 
 import "./style.scss"
+import classnames from "classnames"
 
-const TimeButtons = ({theTime, setYourTime}) => {
+const TimeButtons = ({theTime, setYourTime, makeActive}) => {
     
     return (
         <div className="stage__time">
             {
-                theTime.map(time => <button className="stage-time-selection" key={time.realTime} 
-                onClick={() => setYourTime(time.realTime)}>{time.textTime}</button>)
+                theTime.map(time => <button disabled={!makeActive} 
+                    className={classnames("stage-time-selection", makeActive ? "active-button" : "non-active-button")} 
+                key={time.realTime} onClick={() => setYourTime(time.realTime)}>{time.textTime}</button>)
             }
             <div></div>
         </div>
