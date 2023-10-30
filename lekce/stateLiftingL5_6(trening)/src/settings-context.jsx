99f796a080ce2d2f308cdd019aacf980ed69ce17
LipusM <mlipus007@gmail.com>
2023-10-30 19:41:13 +0100

    Komentování

--- lekce/stateLiftingL5_6(trening)/src/settings-context.jsx
+++ lekce/stateLiftingL5_6(trening)/src/settings-context.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react"
 
-//Tvorba samotného contextu (resp. vytvářím to místo v paměti)
+//Tvorba samotného contextu (resp. vytvářím to místo v paměti). Resp. konstruktor contextu.
 export const SettingsContext = createContext()
 
 //Tvorba vlastního hooku pro volání useContext (resp. fce používající v sobě jiný hook). Pro zjednodušení zápisu přístupu ke stavu.
 //Custom hooky vyt. proto, aby měly větší vypovídající hodnotu. Aby nám lépe sdělily co ten kus kódu má dělat. 
 //Když potřebujeme, aby dělal další věci, proto je lepší custom hook.
-export const useSettings = () => useContext(SettingsContext)
+export const useSettings = () => useContext(SettingsContext) //Použití vlastního hooku, a jako hodnotu dáváme jaký context chceme použít.
 
 
 
