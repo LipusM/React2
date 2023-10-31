63644388fac7c88591db442ff10deedb7f726782
LipusM <mlipus007@gmail.com>
2023-10-31 22:09:37 +0100

    Formátování

--- Cviceni/spravaStavuL6 (kopie)/src/components/Header/index.jsx
+++ Cviceni/spravaStavuL6 (kopie)/src/components/Header/index.jsx
@@ -1,31 +1,29 @@
-const c = console.log.bind(document)
+const c = console.log.bind(document);
 
-import { usePrefs } from '../../prefs-context'
-import './style.scss'
+import { usePrefs } from "../../prefs-context";
+import "./style.scss";
 
 const Header = () => {
+  const { handleVegan } = usePrefs();
 
-    const {handleVegan} = usePrefs()
-
-    const handleChange = (e) => {
-        if(e.target.value === "false"){
-            handleVegan(false)
-        } else {
-            handleVegan(true)
-        }
+  const handleChange = (e) => {
+    if (e.target.value === "false") {
+      handleVegan(false);
+    } else {
+      handleVegan(true);
     }
+  };
 
-    return(
-        <header>
-        <div className="pizza" />
-        <h1>Build your own pizza</h1>
-        <select onChange={handleChange}>
-            <option value={true}>Vegan only</option>
-            <option value={false}>All</option>
-        </select>
-        </header>
-    )
-
+  return (
+    <header>
+      <div className="pizza" />
+      <h1>Build your own pizza</h1>
+      <select onChange={handleChange}>
+        <option value={true}>Vegan only</option>
+        <option value={false}>All</option>
+      </select>
+    </header>
+  )
 }
 
-export default Header
+export default Header;
