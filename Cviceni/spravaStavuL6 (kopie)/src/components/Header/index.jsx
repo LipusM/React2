1b9c97f3641cc0216990fca79c4b3d34b2da1096
LipusM <mlipus007@gmail.com>
2023-10-31 19:43:30 +0100

    Projekt vrácen do původního stavu v rámci přípravy na cvičení.

---
+++ Cviceni/spravaStavuL6 (kopie)/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+const c = console.log.bind(document)
+
+import './style.scss'
+
+const Header = () => {
+
+    return(
+        <header>
+        <div className="pizza" />
+        <h1>Build your own pizza</h1>
+        </header>
+    )
+
+}
+
+export default Header
