91656d4f163f3256cd126973e4c545f9186cd7f7
LipusM <mlipus007@gmail.com>
2023-11-01 18:06:54 +0100

    ..

---
+++ Cviceni/emoticon-zadani-main/src/components/ColorSelector/index.jsx
@@ -0,0 +1,23 @@
+import {colorsData} from '../../data';
+
+const ColorSelector = () => {
+
+	const handleClick = (item) => {
+		console.log('color', item.id);
+	}
+
+	return (
+		<div className="items">
+			{colorsData.map(color =>
+				<div
+					className='item'
+					key={color.id}
+					style={{ backgroundColor: color.value}}
+					onClick={() => { handleClick(color) }}
+					/>
+			)}
+		</div>
+	);
+}
+
+export default ColorSelector;
