7c7d1c2fb3e026692ba0d1ff47dd27cadf257f8d
LipusM <mlipus007@gmail.com>
2023-11-02 20:59:29 +0100

    Přehrávání

--- Cviceni/karraokePrehravacL7/src/App.jsx
+++ Cviceni/karraokePrehravacL7/src/App.jsx
@@ -2,13 +2,16 @@ const c = console.log.bind(document)
 
 import './app.scss'
 
+import theSong from "./fools-garden-lemon-tree.mp3" //Klasický import
+import Player from './components/Player'
+
 const App = () => {
 
 
   return (
-    <>
-
-    </>
+    <div className='container'>
+        <Player src="/fools-garden-lemon-tree.mp3"/>
+    </div>
   )
 }
 
---
+++ Cviceni/karraokePrehravacL7/src/components/Player/index.jsx
@@ -0,0 +1,49 @@
+const c = console.log.bind(document)
+
+import { useState, useEffect, useRef } from "react"
+import classnames from "classnames"
+
+const Player = ({src}) => {
+
+    const [playing, setPlaying] = useState(false)
+    const audioRef = useRef()
+
+    const handleMusic = () => {
+        setPlaying(!playing)
+    }
+
+    useEffect(() => {
+        if(playing){
+            audioRef.current.play()
+        } else {
+            audioRef.current.pause()
+        }
+    }, [playing])
+
+    return (
+        <div class="container">
+        <h1>Fools Garden: Lemon Tree</h1>
+        <audio src={src} ref={audioRef}></audio>
+        <div class="player-controls">
+          <button onClick={handleMusic} 
+          className={classnames("play-button", {
+              play: !playing,
+              pause: playing,
+          })}></button>
+        </div>
+        <div class="lyrics">
+          <p>I'm sittin' here in the boring room</p>
+          <p>It's just another rainy Sunday afternoon</p>
+          <p>I'm wasting my time I got nothin' to do</p>
+          <p>I'm hangin' around I'm waitin' for you</p>
+          <p>But nothing ever happens and I wonder</p>
+          <p>I'm drivin' around in my car</p>
+          <p>I'm drivin' too fast I'm drivin' too far</p>
+          <p>I'd like to change my point of view</p>
+          <p>I feel so lonely I'm waitin' for you</p>
+        </div>
+      </div>
+    )
+}
+
+export default Player
