35f8d2d6f03fad8996af38eea6982b2c0cf47dff
LipusM <mlipus007@gmail.com>
2023-11-04 11:00:38 +0100

    komentování

--- Cviceni/karraokePrehravacL7/src/App.jsx
+++ Cviceni/karraokePrehravacL7/src/App.jsx
@@ -3,13 +3,17 @@ const c = console.log.bind(document)
 import './app.scss'
 
 import Player from './components/Player'
+import Lyrics from './components/Lyrics'
+
+import songText from "../src/lyrics-lines"
 
 const App = () => {
 
 
   return (
     <div className='container'>
-        <Player src="/fools-garden-lemon-tree.mp3"/>
+        <Player src="/fools-garden-lemon-tree.mp3" lines={songText}/>
+        {/* <Lyrics lines={songText}/> */}
     </div>
   )
 }
