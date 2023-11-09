13b14f798d255c0d9d9e9e55f01f0a902e5d95e1
LipusM <mlipus007@gmail.com>
2023-11-09 19:04:35 +0100

    ..

--- lekce/formulareL8/src/App.jsx
+++ lekce/formulareL8/src/App.jsx
@@ -2,12 +2,15 @@ const c = console.log.bind(document)
 
 import './app.scss'
 
+import Registration from './components/Registration'
+import BetterRegistration from './components/BetterRegistration'
+
 const App = () => {
   
 
   return (
     <>
-
+      <BetterRegistration />
     </>
   )
 }
---
+++ lekce/formulareL8/src/components/BetterRegistration/index.jsx
@@ -0,0 +1,75 @@
+const c = console.log.bind(document);
+
+import "./style.scss";
+import { useFormik } from "formik";
+
+const validateForm = (values) => {
+  const errors = {}
+
+  if(!values.firstName){
+    errors.firstName = "Povinné pole"
+  } else if (values.firstName.length > 15){
+    errors.firstName = "Může obsahovat max. 15 znaků"
+  }
+
+  if(!values.lastName){
+    errors.lastName = "Povinné pole"
+  } else if (values.lastName.length > 15){
+    errors.lastName = "Může obsahovat max. 15 znaků"
+  }
+
+  return errors
+}
+
+
+
+
+
+
+const BetterRegistration = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+    },
+    validate: validateForm,
+    onSubmit:(data) => c(data)
+  })
+
+
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label htmlFor="firstname">Jméno</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.firstName && formik.errors.firstName && <p>{formik.errors.firstName}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="lastname">Příjmení</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.lastName && formik.errors.lastName && <p>{formik.errors.lastName}</p>}
+
+        <button type="submit">Odeslat</button>
+      </div>
+    </form>
+  );
+};
+
+export default BetterRegistration;
