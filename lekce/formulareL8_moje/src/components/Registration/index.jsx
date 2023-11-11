6b859cf43ed75f3c9940d4fc4b3316b5967db4a5
LipusM <mlipus007@gmail.com>
2023-11-11 11:54:38 +0100

    ..

---
+++ lekce/formulareL8_moje/src/components/Registration/index.jsx
@@ -0,0 +1,42 @@
+const c = console.log.bind(document);
+
+import "./style.scss";
+
+import useForm from "../../hooks/useForm";
+
+const Registration = () => {
+
+    const {formData, handleChange, handleSubmit} = useForm({
+        firstName: "",
+        lastName: "",
+    }, data => {c(data)})
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="firstname">Jméno</label>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+      />
+
+      <div></div>
+
+      <label htmlFor="lastname">Příjmení</label>
+      <input
+        type="text"
+        id="lastName"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+      />
+
+      <button type="submit">Odeslat</button>
+    </form>
+  )
+}
+
+export default Registration;
---
+++ lekce/formulareL8_moje/src/hooks/useForm.jsx
@@ -0,0 +1,21 @@
+const c = console.log.bind(document)
+
+import { useState } from "react";
+
+const useForm = (initialValue = {}, onSubmit) => {
+  const [formData, setFormData] = useState(initialValue)
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    onsubmit(formData)
+  }
+
+  return {formData, handleChange, handleSubmit}
+}
+
+export default useForm;
